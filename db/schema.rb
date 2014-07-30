829e5743b00f9844adf03de66e0c0587f08e5de5
randallreedjr <randallreedjr@gmail.com>
2014-07-30 11:24:10 -0400

    Create migration for artists table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140730020244) do
+ActiveRecord::Schema.define(version: 20140730152303) do
+
+  create_table "artists", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "requests", force: true do |t|
     t.string   "city"
