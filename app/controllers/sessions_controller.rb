1cdb7761a11a5c7d1a3f9deea9c8d711192dcd32
randallreedjr <randallreedjr@gmail.com>
2014-07-30 11:20:43 -0400

    Store user's name and display on welcome screen

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,8 @@ class SessionsController < ApplicationController
 
   def create 
      
-    auth = request.env["omniauth.auth"]     
+    auth = request.env["omniauth.auth"]    
+    binding.pry 
     user = User.find_by_provider_and_uid(auth[:provider], auth[:uid]) || User.create_from_omniauth(auth)     
     session[:user_id] = user.id     
     redirect_to root_url, :notice => "Signed in!"
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ActiveRecord::Base
 
  def self.create_from_omniauth(auth_hash)
   self.create(provider: auth_hash[:provider],
-              uid: auth_hash[:uid]
-
+              uid: auth_hash[:uid],
+              name: auth_hash[:info][:name]
               )
 end
 
