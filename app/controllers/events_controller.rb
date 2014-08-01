049098a17c398018b67344896d8c5957b8bbcd8e
randallreedjr <randallreedjr@gmail.com>
2014-08-01 10:47:50 -0400

    Add top events page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,4 +5,16 @@ class EventsController < ApplicationController
   def index
     @events = Event.joins(:event_users).where(:event_users => {:user_id => current_user.id}).order(:datetime)
   end
+
+  def top
+      address = Geocoder.address(remote_ip).split(', ')
+      if address[2] == "United States"
+        city = address[0]
+        state = address[1][0..1]
+        @location = "#{city}, #{state}"
+      else
+        @location = ""
+      end
+    @top_events = Event.top_events
+  end
 end
