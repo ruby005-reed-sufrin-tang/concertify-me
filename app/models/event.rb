75782d9587a6c25b979a84557e0ccca5ce4a8ddb
randallreedjr <randallreedjr@gmail.com>
2014-08-01 13:36:33 -0400

    Can save top events to personal event list

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,6 +7,6 @@ class Event < ActiveRecord::Base
   has_many :artists, through: :artist_events
 
   def self.top_events
-    ActiveRecord::Base.connection.execute("SELECT events.title, count(*) as count FROM events JOIN event_users ON event_users.event_id = events.id WHERE events.location = 'New York, NY' GROUP BY events.title ORDER BY count DESC LIMIT 5")
+    ActiveRecord::Base.connection.execute("SELECT events.title, events.id, count(*) as count FROM events JOIN event_users ON event_users.event_id = events.id WHERE events.location = 'New York, NY' GROUP BY events.title, events.id ORDER BY count DESC LIMIT 5")
   end
 end
